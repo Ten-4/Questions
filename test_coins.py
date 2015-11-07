74dceaa078e4329d8d69e1048a78e50da20db73e
Tommi Pesu <tommi.pesu13@imperial.ac.uk>
2015-11-07 10:51:59 +0000

    coins tests

--- test_coins.py
+++ test_coins.py
@@ -1,44 +1,49 @@
-from coins import coin_solver
-
-def acc(L,n):
-	if n < 0:
-		return 0
-	else:
-		return L[n]
+from solution import coin_solver
 
 def correct_coin_solver(coins,n):
-	L = [0]*(n+1)
-	L[0] = 1
-	for i in xrange(1,n+1):
-		for i in coins:
-			L[i] += acc(L,i-coins)
-	return L[n]
+	coins.sort()
+	if n == 0:
+		return 1
+	if len(coins) == 0 and n != 0:
+		return 0
+	
+	H[ (0,0) ] = 1
+	for num in range(1,n+1):
+		for lcoin in coins:
+			H[(num,lcoin)] = 0
 			
-			
-def test_0():
+			for val in coins:
+				if val >= lcoin and (num,lcoin-val) in H:
+					H[(num,lcoin)] += H[(num,lcoin-val)]
+	total = 0
+	for val in coins:
+		if (n,val) in H:
+			total += H[(n,val)]
+	return total
+	
+def test_empty0():
+	coins = []
+	n = 0
+	print correct_coin_solver(coins,n)
+	assert coin_solver(coins,n) == correct_coin_solver(coins,n)
+
+def test_empty1():
+	coins = []
+	n = 1
+	print correct_coin_solver(coins,n)
+	assert coin_solver(coins,n) == correct_coin_solver(coins,n)
+
+	
+def test_uk0():
 	coins = [1,2,5,10,20,50,100,200]
 	n = 0
+	print correct_coin_solver(coins,n)
 	assert coin_solver(coins,n) == correct_coin_solver(coins,n)
 	
-def test_1_100():
-	coins = [1]
-	n = 100
+def test_uk200	
+	coins = [1,2,5,10,20,50,100,200]
+	n = 200
+	print correct_coin_solver(coins,n)
 	assert coin_solver(coins,n) == correct_coin_solver(coins,n)
-	
-def test_1251050100_1000():	
-	coins = [1,2,5,10,50,100]
-	n = 1000
-	assert coin_solver(coins,n) == correct_coin_solver(coins,n)
-	
-def test_1237_100():	
-	coins = [1,2,3,7]
-	n = 100
-	assert coin_solver(coins,n) == correct_coin_solver(coins,n)
-	
-def test_empty_10:
-	coins = []
-	n = 100
-	assert coin_solver(coins,n) == correct_coin_solver(coins,n)
-	
 	
 	
